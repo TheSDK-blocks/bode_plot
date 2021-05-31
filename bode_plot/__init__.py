b5bf6d0fcf713e7cc8e789fb5d6338dca4e2229e
Santeri Porrasmaa <santeri.porrasmaa@aalto.fi>
2021-05-31 17:04:44 +0300

    Add separate plotting for magnitude and phase

--- bode_plot/__init__.py
+++ bode_plot/__init__.py
@@ -41,6 +41,8 @@ from thesdk import *
 from rtl import *
 from spice import *
 import matplotlib.pyplot as plt
+import matplotlib
+from matplotlib.ticker import MultipleLocator, AutoMinorLocator, LogLocator
 import numpy as np
 
 class bode_plot(rtl,spice,thesdk):
@@ -75,12 +77,13 @@ class bode_plot(rtl,spice,thesdk):
         self.IOS.Members['vin']=IO() # Pointer for input data
         self.IOS.Members['vout']=IO()
         self.calc_tf=False # internally controlled variable
+        self.freq=None # Frequency axis data used in plotting
         self.tf=None # The transfer function is stored in this variable
         self.mag_plot=True # Draw magnitude plot?
         self.phase_plot=True # Draw phase plot?
         self.annotate_cutoff=True # Annotate cut-off frequency?
-        self.mag_label='$|H(j\omega)|$' # Label for magnitude plot
-        self.arg_label='$\angle H(j\omega)$' # Label for phase plot
+        self.mag_label=None # Label for magnitude plot
+        self.phase_label=None # Label for phase plot
         self.plot_title = 'Bode plot'
         self.resp_type = 'LP' # Expected response type
         self.degrees=False # Calculate argument of transfer function in degrees?
@@ -106,33 +109,24 @@ class bode_plot(rtl,spice,thesdk):
         """
         pass #Currently nohing to add
 
-    def get_cutoff(self, freq, magdata):
+    def get_cutoff(self, magdata):
         cutoff_level = -6 # TODO: Add this as parameter?
         arr=np.abs(magdata-cutoff_level)
         if self.resp_type.lower()=='lp' or self.resp_type.lower()=='hp':
             idx1=arr.argmin()
-            return [freq[idx1]]
+            return [self.freq[idx1]]
         elif self.resp_type.lower()=='bp':
             idx1 = arr.argmin()
             arr=np.delete(arr, idx1)
             idx2 = arr.argmin()
-            return [freq[idx1], freq[idx2]]
+            return [self.freq[idx1], self.freq[idx2]]
         else:
-            self.print_log(type='F', msg='Unsupport response type %s' % self.resp_type)
+            self.print_log(type='F', msg='Unsupported response type %s' % self.resp_type)
 
-
-    def main(self):
-        ''' The main python description of the operation. Contents fully up to designer, however, the 
-        IO's should be handled bu following this guideline:
-        
-        To isolate the internal processing from IO connection assigments, 
-        The procedure to follow is
-        1) Assign input data from input to local variable
-        2) Do the processing
-        3) Assign local variable to output
-
+    def check_input(self):
         '''
-        # Input signal processing, check dimensions are correct, etc..
+        Check that the input is of correct format.
+        '''
         vout_mat=self.IOS.Members['vout'].Data
         vin_mat=self.IOS.Members['vin'].Data
         nrows,ncols=vout_mat.shape
@@ -163,47 +157,100 @@ class bode_plot(rtl,spice,thesdk):
                 freq=freq[0:maxlen]
         else: # Otherwise, plot only vout
             self.calc_tf=False
+        self.freq=freq
         if self.calc_tf:
             self.tf=vout/vin
         else:
             self.tf=vout
-        
-        mag_data=20*np.log10(np.abs(self.tf))
-        arg_data=np.angle(self.tf, deg=self.degrees)
+        return
 
+
+    def main(self):
+        ''' The main python description of the operation. Contents fully up to designer, however, the 
+        IO's should be handled bu following this guideline:
+        
+        To isolate the internal processing from IO connection assigments, 
+        The procedure to follow is
+        1) Assign input data from input to local variable
+        2) Do the processing
+        3) Assign local variable to output
+
+        '''
+        keys=sys.modules.keys()
+        if 'plot_format' in keys and self.mag_plot and self.phase_plot:
+            self.print_log(type='W', msg='plot_format module detected! Using plot_format may provide poor results when plotting mag. and phase to same plot!')
+        # Input signal processing, check dimensions are correct, etc..
+        # After calling this, transfer function and frequency points are 
+        # available from their respective variables (see documentation)
+        self.check_input()
+          
+        # Calculate magnitude and phase from transfer function
+        mag_data=20*np.log10(np.abs(self.tf))
+        phase_data=np.angle(self.tf, deg=self.degrees)
+        # Get cutoff frequency
         if self.annotate_cutoff:
-            cutoff=self.get_cutoff(freq,mag_data)
+            cutoff=self.get_cutoff(mag_data)
             for f in cutoff:
                 self.print_log(type='I', msg='Cut-off frequency is: %.4g Hz.' % f)        
+        # Update labels, if not already given:
+        if self.mag_label is None:
+            self.mag_label='Magnitude (dB)'
+        if self.phase_label is None:
+            if self.degrees:
+                self.phase_label='Phase (deg.)'
+            else:
+                self.phase_label='Phase (rad.)'
+        # Set x-axis limits based on data, if not already given
         if not self.xlim:
-            self.xlim=(freq[0], freq[-1])
-
+            self.xlim=(self.freq[0], self.freq[-1])
         if self.mag_plot and self.phase_plot: 
             fig, ax = plt.subplots(2,1,sharex=True)
-            subfig1=ax[0].semilogx(freq.real,mag_data)
+            subfig1=ax[0].plot(self.freq,mag_data)
             ax[0].set_ylabel(self.mag_label) 
-            ax[0].grid(True)
-            ax[0].set_ylim(bottom=min(mag_data))
-            ax[0].xlim=self.xlim
+            ax[0].set_xscale('log')
+            ax[0].set_xlim(*self.xlim)
             if self.annotate_cutoff:
                 cutoff=sorted(cutoff)
+                ax[0].set_ylim(bottom=min(mag_data)) # This avoids vertical line from strecting the y-limit
                 for i,f in enumerate(cutoff):
                     ax[0].axvline(x=f, linestyle='--')
                     plt.text(0.5,0.05+0.2*i, '$f_{c%d}=%.2g$' % (i,f),transform=ax[0].transAxes)
-            subfig2=ax[1].semilogx(freq.real,arg_data)
-            ax[1].set_ylabel(self.arg_label) 
+            subfig2=ax[1].plot(self.freq,phase_data)
+            ax[1].set_ylabel(self.phase_label) 
             ax[1].set_xlabel('Frequency (Hz)')
-            ax[1].xlim=self.xlim
+            ax[1].set_xlim(*self.xlim)
+            ax[1].set_xscale('log')
             ax[1].grid(True) 
             fig.suptitle(self.plot_title)
             plt.show(block=False)
             if self.save_fig:
                 plt.savefig(self.save_path,format='pdf')
-        # TODO: Implement
         elif self.mag_plot and not self.phase_plot:
-            pass
+            fig=plt.plot(self.freq, mag_data)
+            ax=plt.gca()
+            ax.set_ylabel(self.mag_label)
+            ax.set_xlabel('Frequency (Hz)')
+            ax.set_xscale('log')
+            ax.set_xlim(*self.xlim) 
+            if self.annotate_cutoff:
+                cutoff=sorted(cutoff)
+                ax.set_ylim(bottom=min(mag_data)) # This avoids vertical line from extending the y-limit
+                for i,f in enumerate(cutoff):
+                    ax.axvline(x=f, linestyle='--')
+                    plt.text(0.5,0.05+0.2*i, '$f_{c%d}=%.2g$' % (i,f),transform=ax.transAxes)
+            plt.show(block=False)
+            if self.save_fig:
+                plt.savefig(self.save_path,format='pdf')
         elif self.phase_plot and not self.mag_plot:
-            pass
+            fig=plt.plot(self.freq, phase_data)
+            ax=plt.gca()
+            ax.set_ylabel(self.phase_label)
+            ax.set_xlabel('Frequency (Hz)')
+            ax.set_xscale('log')
+            ax.set_xlim(*self.xlim) 
+            plt.show(block=False)
+            if self.save_fig:
+                plt.savefig(self.save_path,format='pdf')
         else:
             self.print_log(type='I', msg='mag_plot and phase_plot flags were false: no plots produced!')
     def run(self,*arg):
